1cc4e8fecb06669bf2b760ee7bad0fdd7a18b5ff
Xuefen Li <xuefenli@live.com>
2019-02-12 18:30:50 -0800

    added in home page with Hawaii image

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <title>CSCI E-15 Project 1</title>
+   <meta charset="utf-8">
+</head>
+<body>
+   <h1>My name is Xuefen Li</h1>
+   <img src='/images/hawaii.jpg' alt='Honolulu, Hawaii'>
+   
+   <h1>Bio</h1>
+   <p>...</p>
+   
+</body>
+</html>
